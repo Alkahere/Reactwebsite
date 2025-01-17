328a16f42fca4b1b6b58f07cc8f4906a76b5ac69
Alkahere <138251514+Alkahere@users.noreply.github.com>
2025-01-18 00:38:12 +0530

    about page , marquee page

--- ochi/src/App.jsx
+++ ochi/src/App.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import Navbar from './components/navbar'
 import LandingPage from './components/LandingPage'
+import Marquee from './components/Marquee'
+import About from './components/About'
+import Eyes from './components/Eyes'
+
 function App() {
   return (
-    <div className="w-full h-screen text-white">
+    <div className="w-full text-white min-h-screen bg-zinc-900">
     <Navbar />
     <LandingPage />
+    <Marquee />
+    <About />
+    <Eyes />
 
     </div>
   )
--- ochi/src/components/LandingPage.jsx
+++ ochi/src/components/LandingPage.jsx
@@ -8,22 +8,24 @@ function LandingPage() {
         <div className='textstrcture mt-52 px-20'>
           {["We Create", "Eye Opening", "Presentations"].map((item, index) => (
             <div key={index} className='masker'>
-              <h1 className="uppercase text-[7.5vw] leading-[6vw] tracking-tighter font-['FoundersGrotesk-Semibold'] font-medium">{item}</h1>
+                <div className="w-fit flex items-end overflow-hidden">
+                    {index=== 1 && (<div className="mr-5 w-[9vw] rounded-md h-[6vw] -top-[2.2vw] relative bg-red-500"></div>)}
+              <h1 className="uppercase text-[7vw] leading-[6vw] tracking-tighter font-['Test_Founders_Grotesk_Condensed'] font-medium">{item}</h1>
+            </div>
             </div>
           ))}
         </div>
-{/* Section with text below hr and arrow */}
-<div className="border-t-[1px] border-zinc-800 mt-32 flex items-center py-5 px-20 justify-between">
+<div className="border-t-[2px] border-zinc-800 mt-20 flex items-center py-2 px-20 justify-between">
         {/* Text content */}
         {["For public and private companies", "From the first pitch to IPO"].map((item, index) => (
-          <p key={index} className="text-md font-light tracking-tight leading-none mr-10">
+          <p key={index} className="text-md font-light mr-10">
             {item}
           </p>
         ))}
         
         {/* Arrow button */}
         <div className="start flex items-center gap-4">
-        <div className="px-5 py-2 border-[2px] border-zinc-400 font-light text-md uppercase flex items-center justify-center">
+        <div className="px-5 py-2 border-[2px] border-zinc-400 font-light text-md uppercase flex items-center justify-center">start project
           <div className="w-10 h-10 flex items-center justify-center border-[2px] border-zinc-400 rounded-full">
             <span className="rotate-[45deg]">
               <FaArrowUpLong />
@@ -32,8 +34,7 @@ function LandingPage() {
           </div>
         </div>
       </div>
-
-    </div>
+</div>
   );
 }
 
